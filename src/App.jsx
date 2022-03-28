b1d86c076d7a3251b61fecc0f32df71d3205ee7c
Admin <admin@Admins-iMac-3.local>
2022-03-28 19:02:48 +0530

    used class component and toggle text hide/show

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Edittext from "./components/Edittext";
+import Profile from "./Profile";
 function App() {
   return (
     <>
       <Navbar />
       <Edittext />
+      <Profile />
     </>
   );
 }
