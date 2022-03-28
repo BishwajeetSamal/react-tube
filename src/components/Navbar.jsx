ad04b14c7134981e01bd2f7e2e2d1221ef5f8d6e
Admin <admin@Admins-iMac-3.local>
2022-03-28 14:27:00 +0530

    image add using public_url

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  var name = "Bishwajeet Samal";
+  var ImgAddr = process.env.PUBLIC_URL + "/images/train.jpeg";
+  return (
+    <>
+      <h1>{name} Test</h1>
+      <img src={ImgAddr} alt="Train Image" />
+    </>
+  );
+}
+
+export default Navbar;
