f69650ac03ef973098314ad1983a1ae2f4f9d7ea
Admin <admin@Admins-iMac-3.local>
2022-04-06 14:25:13 +0530

    applying the css from js

--- src/ReactRouter/NavbarOne.js
+++ src/ReactRouter/NavbarOne.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 function NavbarOne() {
+  const navStyleLinks = ({ isActive }) => {
+    return {
+      fontWeight: isActive ? "bold" : "normal",
+      color: isActive ? "lightseagreen" : "",
+    };
+  };
   return (
     <div>
       <nav>
-        <NavLink to="/home">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
+        <NavLink style={navStyleLinks} to="/home">
+          Home
+        </NavLink>
+        <NavLink style={navStyleLinks} to="/about">
+          About
+        </NavLink>
       </nav>
     </div>
   );
