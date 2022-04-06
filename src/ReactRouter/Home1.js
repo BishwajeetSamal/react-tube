0df15b41828009f0e97582fd965804934d0d23cb
Admin <admin@Admins-iMac-3.local>
2022-04-06 15:05:39 +0530

    order summary navigate by button in homepage

--- src/ReactRouter/Home1.js
+++ src/ReactRouter/Home1.js
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 function Home1() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Home Page</h1>
+      <button onClick={() => navigate("/order-summary")}>Place Order</button>
     </div>
   );
 }
--- src/ReactRouter/mainApp.js
+++ src/ReactRouter/mainApp.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home1 from "./Home1";
 import About from "./About";
 import NavbarOne from "./NavbarOne";
+import OrderSummary from "./OrderSummary";
 function mainApp() {
   return (
     <div>
@@ -10,6 +11,7 @@ function mainApp() {
       <Routes>
         <Route path="/home" element={<Home1 />} />
         <Route path="/about" element={<About />} />
+        <Route path="/order-summary" element={<OrderSummary />} />
       </Routes>
     </div>
   );
