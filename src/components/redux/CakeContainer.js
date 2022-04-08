4d3608a6c1982c2aae2ab702816ec332988b49aa
Admin <admin@Admins-iMac-3.local>
2022-04-08 14:26:01 +0530

    redux folder create and add to navbar

---
+++ src/components/redux/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function CakeContainer() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2>Number of Cakes</h2>
+      <button>Buy Cake</button>
+    </div>
+  );
+}
+
+export default CakeContainer;
---
+++ src/components/redux/RenderRedux.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CakeContainer from "./CakeContainer";
+
+function RenderRedux() {
+  return (
+    <div>
+      <CakeContainer />
+    </div>
+  );
+}
+
+export default RenderRedux;
