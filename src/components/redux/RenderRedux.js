40139c1b2792e1943b77f94567508da052a78072
Admin <admin@Admins-iMac-3.local>
2022-04-08 16:33:58 +0530

    state redux wok

--- src/components/redux/RenderRedux.js
+++ src/components/redux/RenderRedux.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Provider } from "react-redux";
 import CakeContainer from "./CakeContainer";
-
+import store from "./store";
 function RenderRedux() {
   return (
-    <div>
+    <Provider store={store}>
       <CakeContainer />
-    </div>
+    </Provider>
   );
 }
 
